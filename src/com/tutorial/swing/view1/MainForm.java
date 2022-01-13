1b88eea8923b9a21c56e0cb90ea3a58d4a529c98
wiwin.ristanto <wiwinristanto94@gmail.com>
2022-01-13 17:24:59 +0700

    style : perbaikan instance

--- src/com/tutorial/swing/view1/MainForm.java
+++ src/com/tutorial/swing/view1/MainForm.java
@@ -1,8 +1,14 @@
 package com.tutorial.swing.view1;
 
+import com.sun.istack.internal.Nullable;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+interface Action {
+    public void receiveData1(String firstname, String LastName, String age);
+}
 
 public class MainForm {
     private JTextField tfFirstName;
@@ -11,25 +17,31 @@ public class MainForm {
     private JButton button2;
     private JPanel rootPanel;
     private JButton openJOptionPane;
+    @Nullable
+    private Action action;
+    private TargedForm targedForm;
+
+    public void setListener(Action listener) {
+        action = listener;
+    }
 
     public MainForm() {
+        targedForm = new TargedForm();
+        targedForm.createLayout();
         openJOptionPane.addActionListener(actionEvent -> {
-            JOptionPane.showMessageDialog(rootPanel, "Hello JOptionPane", "Information" ,JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(rootPanel, "Hello JOptionPane", "Information", JOptionPane.INFORMATION_MESSAGE);
             JOptionPane.showMessageDialog(rootPanel, "Error on message", "Error", JOptionPane.ERROR_MESSAGE);
             int result = JOptionPane.showConfirmDialog(rootPanel, "Are you sure want to continue ?");
-            if (result == JOptionPane.YES_OPTION){
-                String message =JOptionPane.showInputDialog("Input Your Message");
+            if (result == JOptionPane.YES_OPTION) {
+                String message = JOptionPane.showInputDialog("Input Your Message");
                 JOptionPane.showMessageDialog(rootPanel, "Inputted Message : " + message, "information", JOptionPane.INFORMATION_MESSAGE);
             }
         });
 
-
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                TargedForm targedForm = new TargedForm();
-                targedForm.createLayout();
-                targedForm.receiveData(tfFirstName.getText(), tfLastName.getText(),tfAge.getText());
+        button2.addActionListener(actionEvent -> {
+            targedForm.receiveData(tfFirstName.getText(), tfLastName.getText(), tfAge.getText());
+            if (action != null) {
+//                action.receiveData1(tfFirstName.getText(), tfLastName.getText(), tfAge.getText());
             }
         });
     }
--- src/com/tutorial/swing/view1/TargedForm.java
+++ src/com/tutorial/swing/view1/TargedForm.java
@@ -2,20 +2,32 @@ package com.tutorial.swing.view1;
 
 import javax.swing.*;
 
-public class TargedForm {
+public class TargedForm implements Action {
     private JPanel rootTarged;
     private JLabel lblFirstNAme;
     private JLabel lblLastName;
     private JLabel lblAge;
+//    private MainForm mainForm = new MainForm();
+    private JFrame frame;
 
     public void createLayout(){
-        JFrame JFrame = new JFrame("Target Form");
-        JFrame.setContentPane(rootTarged);
-        JFrame.pack();
-        JFrame.setLocationRelativeTo(null);
-        JFrame.setVisible(true);
+//        mainForm.setListener(this);
+        frame = new JFrame("Target Form");
+        frame.setContentPane(rootTarged);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
     }
+
+    @Override
+    public void receiveData1(String firstname, String LastName, String age) {
+        frame.setVisible(true);
+        lblFirstNAme.setText(firstname);
+        lblLastName.setText(LastName);
+        lblAge.setText(age);
+    }
+
     public void receiveData(String firstname, String LastName, String age ){
+        frame.setVisible(true);
         lblFirstNAme.setText(firstname);
         lblLastName.setText(LastName);
         lblAge.setText(age);
